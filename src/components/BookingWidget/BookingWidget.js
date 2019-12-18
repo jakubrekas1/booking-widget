a770c9e98fd5a5231c268c4a03e19eda5a2d3e35
Jakub Rękas <jakubrekas1@gmail.com>
2019-12-18 19:23:37 +0100

    Configure eslint; Add node-sass and prop-types; Init BookingWidget component;

---
+++ src/components/BookingWidget/BookingWidget.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BookingWidget = () => (
+  <div>
+    Some widget...
+  </div>
+);
+
+export default BookingWidget;
---
+++ src/components/BookingWidget/BookingWidgetContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BookingWidget from './BookingWidget';
+
+const BookingWidgetContainer = () => {
+  return (
+    <BookingWidget />
+  );
+};
+
+export default BookingWidgetContainer;
