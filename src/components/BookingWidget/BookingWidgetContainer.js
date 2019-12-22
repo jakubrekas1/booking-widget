cd085cb4b17a3299bfc1e0c2c75f597215651857
Jakub Rękas <jakubrekas1@gmail.com>
2019-12-22 10:10:38 +0100

    Fix imports formatting.

--- src/components/BookingWidget/BookingWidgetContainer.js
+++ src/components/BookingWidget/BookingWidgetContainer.js
@@ -1,4 +1,6 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, {
+  useCallback, useEffect, useRef, useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import { formatDate } from '@codegraphy/react-daterange-picker/services';
 
